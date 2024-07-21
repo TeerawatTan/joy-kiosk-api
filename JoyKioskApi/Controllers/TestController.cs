27b01ed6129a01b45990d0a56f412e06a8877606
TeerawatTan <teerawat.tanatan@gmail.com>
2024-07-21 13:52:10 +0700

    Add Dockerfile

---
+++ JoyKioskApi/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoyKioskApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This test called api and it's ok!");
+        }
+    }
+}
--- JoyKioskApi/Program.cs
+++ JoyKioskApi/Program.cs
@@ -28,11 +28,11 @@ builder.Services.AddCors(o => o.AddPolicy("CorsJoyKioskWebAPI", builder =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
